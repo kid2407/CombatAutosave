b92fce18fe26dc8ae1e5f6e067b54a8b0083d387
Tobias Franz <cinex.mail@web.de>
2020-01-09 18:55:49 +0100

    * Added setting "Autosave Delay" * Fixed various errors * Added Localization for English and German

--- src/control.lua
+++ src/control.lua
@@ -1,22 +1,21 @@
 current_time_without_combat = 0 -- How long currently without combat
-combat_autosave_delay = 60 -- How many ticks until saving
 saved = true -- if we saved after the last fight
-firstRun = true -- dont't do things when the game just loaded
+firstRun = true -- dont't do things when the game just loaded (tick 0)
 
-script.on_nth_tick(30, function()
+-- Handle the autosaving
+function tick_handler()
     if (firstRun == false) then
-        for index, player in pairs(game.connected_players) do
-            if player.in_combat then
-                current_time_without_combat = 0
-                saved = false
-            else
-                if saved == false then
-                    current_time_without_combat = current_time_without_combat + 30
-                    if current_time_without_combat >= combat_autosave_delay then
-                        game.auto_save()
-                        saved = true
-                        current_time_without_combat = 0
-                    end
+        player = game.connected_players[1]
+        if player.in_combat then
+            current_time_without_combat = 0
+            saved = false
+        else
+            if saved == false then
+                current_time_without_combat = current_time_without_combat + 30
+                if current_time_without_combat >= combat_autosave_delay then
+                    game.auto_save()
+                    saved = true
+                    current_time_without_combat = 0
                 end
             end
         end
@@ -24,4 +23,19 @@ script.on_nth_tick(30, function()
         firstRun = false
     end
 end
-)
+
+function loadSettings()
+    combat_autosave_delay = 60 * settings.global["combat_autosave_delay"].value -- How many ticks until saving, time is s * 60 = ticks
+end
+
+-- Read delay initially from settings
+script.on_init(loadSettings)
+script.on_load(loadSettings)
+
+-- read the new value for the delay
+script.on_configuration_changed(function()
+    loadSettings()
+end)
+
+-- Register the tick handler
+script.on_nth_tick(30, tick_handler)
---
+++ src/settings.lua
@@ -0,0 +1,10 @@
+data:extend({
+    {
+        type = "double-setting",
+        name = "combat_autosave_delay",
+        default_value = 3,
+        minimum_value = 0.5,
+        maximum_value = 30,
+        setting_type = "runtime-global"
+    }
+})
